770967d43e25b4c6285bd8a8e5cef4f55e5ce4fe
sumeeth <sumeethveeramallu2001@gmail.com>
2026-05-06 01:00:29 +0530

    changed cargo.toml

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,13 @@
-pub struct ZenUI {}
+mod styles;
+mod widgets;
+
+pub type Result<T> = std::result::Result<T, Error>;
+pub type Error = Box<dyn std::error::Error>;
+
+pub struct UI {}
+
+impl UI {
+    pub fn init() -> Result<Self> {
+        Ok(Self {})
+    }
+}
