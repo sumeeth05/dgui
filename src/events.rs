bce38533738398ec7d50b0967a926783f1256e56
sumeeth <sumeethveeramallu2001@gmail.com>
2026-07-16 21:08:13 +0530

    modified event enums

--- src/events.rs
+++ src/events.rs
@@ -1,26 +1,44 @@
+#[non_exhaustive]
 pub enum Event {
-    Pointer(PointerEvent),
+    Mouse(MouseEvent),
     Keyboard(KeyboardEvent),
 }
 
-pub enum MouseButton {
-    Left,
-    Right,
-}
+#[non_exhaustive]
+pub enum MouseEvent {
+    Move {
+        position: Position,
+    },
 
-pub enum PointerEvent {
-    Move { x: f32, y: f32 },
+    Down {
+        button: MouseButton,
+        position: Position,
+        modifiers: Modifiers,
+    },
 
-    Down { button: MouseButton, x: f32, y: f32 },
+    Up {
+        button: MouseButton,
+        position: Position,
+        modifiers: Modifiers,
+    },
 
-    Up { button: MouseButton, x: f32, y: f32 },
-
-    Scroll { dx: f32, dy: f32 },
+    Scroll {
+        dx: f32,
+        dy: f32,
+    },
 
     Enter,
     Leave,
 }
 
+pub struct Position {
+    x: f64,
+    y: f64,
+}
+
+pub enum MouseButton {}
+
+#[non_exhaustive]
 pub enum KeyboardEvent {
     KeyDown {
         key: Key,
@@ -36,6 +54,145 @@ pub enum KeyboardEvent {
     Text(String),
 }
 
-pub enum Key {}
+#[non_exhaustive]
+pub enum Modifiers {
+    SHIFT,
+    CTRL,
+    ALT,
+    SUPER,
+}
 
-pub enum Modifiers {}
+#[non_exhaustive]
+pub enum Key {
+    A,
+    B,
+    C,
+    D,
+    E,
+    F,
+    G,
+    H,
+    I,
+    J,
+    K,
+    L,
+    M,
+    N,
+    O,
+    P,
+    Q,
+    R,
+    S,
+    T,
+    U,
+    V,
+    W,
+    X,
+    Y,
+    Z,
+
+    Digit0,
+    Digit1,
+    Digit2,
+    Digit3,
+    Digit4,
+    Digit5,
+    Digit6,
+    Digit7,
+    Digit8,
+    Digit9,
+
+    Numpad0,
+    Numpad1,
+    Numpad2,
+    Numpad3,
+    Numpad4,
+    Numpad5,
+    Numpad6,
+    Numpad7,
+    Numpad8,
+    Numpad9,
+
+    Backquote,
+    Backslash,
+    BracketLeft,
+    BracketRight,
+    Comma,
+
+    Minus,
+    Period,
+    Quote,
+    Semicolon,
+    Slash,
+
+    Equal,
+    IntlBackslash,
+    IntlRo,
+    IntlYen,
+
+    AltLeft,
+    AltRight,
+
+    Backspace,
+    CapsLock,
+    ContextMenu,
+
+    ControlLeft,
+    ControlRight,
+
+    Enter,
+
+    SuperLeft,
+    SuperRight,
+
+    ShiftLeft,
+    ShiftRight,
+
+    Space,
+    Tab,
+
+    Delete,
+    End,
+    Home,
+    Insert,
+    ScrollLock,
+    Pause,
+
+    PageDown,
+    PageUp,
+
+    ArrowDown,
+    ArrowLeft,
+    ArrowRight,
+    ArrowUp,
+
+    NumLock,
+
+    NumpadAdd,
+    NumpadBackspace,
+    NumpadClear,
+    NumpadClearEntry,
+    NumpadComma,
+    NumpadDecimal,
+    NumpadDivide,
+    NumpadEnter,
+    NumpadEqual,
+    NumpadMultiply,
+    NumpadSubtract,
+
+    Escape,
+    PrintScreen,
+
+    F1,
+    F2,
+    F3,
+    F4,
+    F5,
+    F6,
+    F7,
+    F8,
+    F9,
+    F10,
+    F11,
+    F12,
+}
